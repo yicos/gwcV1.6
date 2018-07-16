202128de630cc5c9cc4146e6cdd1bdeac5f2b2b2
yicos <13570461024@139.com>
2018-07-16 10:25:20 +0800

    sql

--- gwcV1.6/start.bat
+++ gwcV1.6/start.bat
@@ -1,5 +1,2 @@
-@echo off
-if "%1"=="h" goto begin
-start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
-:begin 
-start jre/bin/java -jar gwcV1.5.jar
+start jre/bin/java -jar gwcV1.6.jar
+pause;
